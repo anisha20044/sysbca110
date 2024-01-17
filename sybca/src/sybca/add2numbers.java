4cc0771be2912c5ee1cf1dfafc2e60e893aaeb30
Mahek Shaikh <mahekshk02@gmail.com>
2024-01-17 10:35:07 +0530

    assignment

---
+++ sybca/src/sybca/add2numbers.java
@@ -0,0 +1,20 @@
+package sybca;
+
+import java.util.Scanner;
+
+public class add2numbers {
+	 public static void main(String args[])   
+	    {  
+	        int sum=0; 
+	        Scanner scan=new Scanner(System.in);
+	        System.out.println("Enter the two numbers");
+	        int num1=scan.nextInt();
+	        int num2=scan.nextInt();
+	        sum = num1 + num2;  
+	        System.out.println("The sum of two numbers is: "+sum);  
+	    }  
+}  
+	
+	
+
+
---
+++ sybca/src/sybca/gitHubDemo.java
@@ -0,0 +1,15 @@
+package sybca;
+
+public class gitHubDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+	void newbranch() {
+		System.out.println("merge and pull");
+		System.out.println("pull");
+	}
+
+}
